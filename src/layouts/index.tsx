348745eef7109ba969cd49be75f2a26962de0307
iamwjun <362934348@qq.com>
2022-11-02 14:56:52 +0800

    chore: update to umi4x

---
+++ src/layouts/index.tsx
@@ -0,0 +1,21 @@
+import { Link, Outlet } from 'umi';
+import styles from './index.less';
+
+export default function Layout() {
+  return (
+    <div className={styles.navs}>
+      <ul>
+        <li>
+          <Link to="/">Hom2e</Link>
+        </li>
+        <li>
+          <Link to="/docs">Docs</Link>
+        </li>
+        <li>
+          <a href="https://github.com/umijs/umi">Github</a>
+        </li>
+      </ul>
+      <Outlet />
+    </div>
+  );
+}
